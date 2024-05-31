3b9da156e077ee76ef8bffffb4261f85bc4e929a
Minh Pham <tuanminhphamtdn@gmail.com>
2024-05-31 12:38:37 +0700

    post list

---
+++ src/app/components/post-list-page/post-create/post-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-post-create',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './post-create.component.html',
+  styleUrl: './post-create.component.css'
+})
+export class PostCreateComponent {
+
+}
---
+++ src/app/components/post-list-page/post-detail/post-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-post-detail',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './post-detail.component.html',
+  styleUrl: './post-detail.component.css'
+})
+export class PostDetailComponent {
+
+}
