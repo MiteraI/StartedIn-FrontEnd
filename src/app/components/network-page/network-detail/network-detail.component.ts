809bbd4ea57d82371daab14bf3f7b7d2d8befdde
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2024-06-02 13:16:47 +0700

    network page

--- src/app/components/network-page/network-detail/network-detail.component.ts
+++ src/app/components/network-page/network-detail/network-detail.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Profile } from '../../../../shared/models/network-profile.model';
+import { NetworkProfileCardComponent } from '../network-profile-card/network-profile-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-network-detail',
   standalone: true,
-  imports: [],
+  imports: [NetworkProfileCardComponent, CommonModule],
   templateUrl: './network-detail.component.html',
-  styleUrl: './network-detail.component.css'
+  styleUrl: './network-detail.component.css',
 })
 export class NetworkDetailComponent {
-
+  profiles: Profile[] = [];
+  constructor() {
+    const profile: Profile = {
+      image: 'https://randomuser.me/api/portraits/men/79.jpg',
+      name: 'John Doe',
+      detail: 'Software Engineer',
+      numberConnections: 500,
+    };
+    this.profiles = new Array<Profile>(12).fill(profile, 0, 12);
+    console.log(this.profiles);
+  }
 }
---
+++ src/shared/models/network-profile.model.ts
@@ -0,0 +1,6 @@
+export interface Profile {
+  image: string;
+  name: string;
+  detail: string;
+  numberConnections: number;
+}
