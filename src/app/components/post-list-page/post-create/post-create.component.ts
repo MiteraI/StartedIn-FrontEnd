f7cd9833a3e808af235ef44ba81864ed48c415c3
Minh Pham <tuanminhphamtdn@gmail.com>
2024-06-10 11:00:54 +0700

    done create post

--- src/app/components/post-list-page/post-create/post-create.component.ts
+++ src/app/components/post-list-page/post-create/post-create.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { CreatePostDialogComponent } from '../../../dialogs/create-post-dialog/create-post-dialog.component';
+import { Account } from '../../../core/auth/account.model';
+import { ReplaySubject, catchError, takeUntil, throwError } from 'rxjs';
+import { AccountService } from '../../../core/auth/account.service';
+import { CreatePost } from '../../../../shared/models/create-post.model';
+import { PostService } from '../../../services/post.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,18 +16,40 @@ import { CreatePostDialogComponent } from '../../../dialogs/create-post-dialog/c
   styleUrl: './post-create.component.css'
 })
 export class PostCreateComponent {
-  content = "";
+  account: Account | null = null;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private accountService: AccountService, private postService: PostService) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(CreatePostDialogComponent, {
-      data: {content: this.content},
+      data: {
+        content: "",
+        postImageFiles: new Array<File>()
+      },
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.content = result;
+      if (!result) {
+        return;
+      }
+      const postData = result as CreatePost;
+      this.postService.createPost(postData).pipe(
+        catchError(error => {
+          return throwError(() => new Error(error));
+        })
+      );
     });
+  }
+
+  ngOnInit() {
+    this.accountService.account$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(account => this.account = account);
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
