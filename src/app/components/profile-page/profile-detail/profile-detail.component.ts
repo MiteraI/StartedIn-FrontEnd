a2ba181cfb6b0124e917c3a7f44313eaa6fa1d58
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2024-06-11 14:06:18 +0700

    edit profile(not photo yet)

--- src/app/components/profile-page/profile-detail/profile-detail.component.ts
+++ src/app/components/profile-page/profile-detail/profile-detail.component.ts
@@ -2,14 +2,26 @@ import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { FullProfileDetail } from '../../../../shared/models/profile/fullProfileDetail.model';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { EditProfileDialogComponent } from '../../../dialogs/edit-profile-dialog/edit-profile-dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile-detail',
   standalone: true,
   imports: [MatIconModule, CommonModule],
   templateUrl: './profile-detail.component.html',
-  styleUrl: './profile-detail.component.css'
+  styleUrl: './profile-detail.component.css',
 })
 export class ProfileDetailComponent {
   @Input() account: FullProfileDetail | null = null;
+
+  constructor(
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(EditProfileDialogComponent, {});
+  }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -5,6 +5,7 @@ import { FullProfileDetail } from '../../shared/models/profile/fullProfileDetail
 import { ApplicationConfigService } from '../core/config/application-config.service';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EditProfile } from '../../shared/models/profile/editProfile.model';
 
 @Injectable({
   providedIn: 'root',
@@ -28,7 +29,19 @@ export class ProfileService {
     };
   }
 
+  mapToEditProfile(profile: AccountProfile): EditProfile {
+    return {
+      content: profile.content,
+      bio: profile.bio,
+      phoneNumber: profile.phoneNumber,
+    };
+  }
+
   getProfile(): Observable<AccountProfile> {
     return this.http.get<AccountProfile>(this.appConfigService.getEndpointFor('/api/full-profile'));
+  }
+
+  edit(editData: EditProfile): Observable<any> {
+    return this.http.put(this.appConfigService.getEndpointFor('/api/profile/edit'), editData);
   }
 }
