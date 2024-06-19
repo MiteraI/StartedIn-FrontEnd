620b032290d6b19aad1ec2b7529e6e164687fe48
Minh Pham <tuanminhphamtdn@gmail.com>
2024-06-19 23:15:04 +0700

    phase dropdown

--- src/app/components/phase-list-page/phase-dropdown/phase-dropdown.component.ts
+++ src/app/components/phase-list-page/phase-dropdown/phase-dropdown.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-phase-dropdown',
+  selector: 'phase-dropdown',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './phase-dropdown.component.html',
   styleUrl: './phase-dropdown.component.css'
 })
 export class PhaseDropdownComponent {
+  expanded: boolean = true;
 
+  toggleExpand() {
+    this.expanded = !this.expanded;
+  }
+
+  showEditPopup(event: Event) {
+    event.stopPropagation();
+  }
+
+  showDeletePopup(event: Event) {
+    event.stopPropagation();
+  }
 }
--- src/app/pages/phase-list-page/phase-list-page.component.ts
+++ src/app/pages/phase-list-page/phase-list-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectTitleCardComponent } from '../../components/phase-list-page/project-title-card/project-title-card.component';
+import { PhaseDropdownComponent } from '../../components/phase-list-page/phase-dropdown/phase-dropdown.component';
 
 @Component({
   selector: 'app-phase-list-page',
   standalone: true,
-  imports: [ProjectTitleCardComponent],
+  imports: [ProjectTitleCardComponent, PhaseDropdownComponent],
   templateUrl: './phase-list-page.component.html',
   styleUrl: './phase-list-page.component.css'
 })
