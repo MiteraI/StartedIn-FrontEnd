50a8b8a56a05c6307866c48bb50859bfc14056e6
Minh Pham <tuanminhphamtdn@gmail.com>
2024-06-19 23:23:43 +0700

    add phase

--- src/app/pages/phase-list-page/phase-list-page.component.ts
+++ src/app/pages/phase-list-page/phase-list-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProjectTitleCardComponent } from '../../components/phase-list-page/project-title-card/project-title-card.component';
 import { PhaseDropdownComponent } from '../../components/phase-list-page/phase-dropdown/phase-dropdown.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-phase-list-page',
   standalone: true,
-  imports: [ProjectTitleCardComponent, PhaseDropdownComponent],
+  imports: [ProjectTitleCardComponent, PhaseDropdownComponent, MatIconModule],
   templateUrl: './phase-list-page.component.html',
   styleUrl: './phase-list-page.component.css'
 })
