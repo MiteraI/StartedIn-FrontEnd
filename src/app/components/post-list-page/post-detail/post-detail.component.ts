f02c2b5345ada75e14faf36841901e07e2534154
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-06-26 07:26:21 +0700

    Platform user profile view complete

--- src/app/components/post-list-page/post-detail/post-detail.component.ts
+++ src/app/components/post-list-page/post-detail/post-detail.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { PostDetail } from '../../../../shared/models/post/post-detail.model';
 import { IsoToVnLocalePipe } from '../../../../shared/pipes/iso-to-locale-string.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [MatIconModule, CommonModule, IsoToVnLocalePipe],
+  imports: [MatIconModule, CommonModule, IsoToVnLocalePipe, RouterLink],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
--- src/app/pages/network-page/platform-user-profile-page/platform-user-profile-page.component.ts
+++ src/app/pages/network-page/platform-user-profile-page/platform-user-profile-page.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AccountProfile } from '../../../../shared/models/profile/profileDetail.model';
-import { JsonPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { ConnectButtonComponent } from '../../../components/network-page/connect-button/connect-button.component';
 
 @Component({
   selector: 'app-platform-user-profile-page',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [MatIconModule, ConnectButtonComponent],
   templateUrl: './platform-user-profile-page.component.html',
   styleUrl: './platform-user-profile-page.component.css',
 })
 export class PlatformUserProfilePageComponent implements OnInit {
-  accountProfile: AccountProfile | null = null;
+  account: AccountProfile | null = null;
   constructor(private activeRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activeRoute.data.subscribe(data => {
-      this.accountProfile = data['account'];
-      console.log('Account profile:', this.accountProfile);
+      this.account = data['account'];
+      console.log('Account profile:', this.account);
     });
   }
 }
